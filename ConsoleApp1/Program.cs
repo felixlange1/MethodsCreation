6dee5120e47267717d228e6c82f056f01d249992
Felix Lange <felixlange1@gmail.com>
2024-12-08 15:45:11 -0800

    circle area method added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    
+    // Method for calculating area of a circle, accepting a radius as parameter:
+    public static double CircleArea(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+    
+    
+}
+
