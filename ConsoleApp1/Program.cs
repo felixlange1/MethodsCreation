2c86e8b4a8e498de076b34980c42070a22cb36ff
Felix Lange <felixlange1@gmail.com>
2024-12-08 15:51:59 -0800

    added triangles area method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,12 @@ class Program
         return Math.PI * radius * radius;
     }
     
-    
+    // Method for calculating area of a triangle, accepting parameters for height and base of the triangle,
+    // which then get multiplied by each other and by 0.5. I used 'baseX' as a variable name for the base,
+    // since it didn't seem to accept 'base':
+    public static double TriangleArea(double height, double baseX)
+    {
+        return 0.5 * height * baseX;
+    }
 }
 
