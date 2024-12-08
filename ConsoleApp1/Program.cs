790640122ba37a00a1e15bc7c2d567872f597def
Felix Lange <felixlange1@gmail.com>
2024-12-08 15:57:40 -0800

    added rectangle area method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,18 +6,25 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
-    
+
     // Method for calculating area of a circle, accepting a radius as parameter:
     public static double CircleArea(double radius)
     {
         return Math.PI * radius * radius;
     }
-    
+
     // Method for calculating area of a triangle, accepting parameters for height and base of the triangle,
     // which then get multiplied by each other and by 0.5:
     public static double TriangleArea(double height, double baseLength)
     {
         return 0.5 * height * baseLength;
     }
-}
 
+
+    // Method for calculating the area of a rectangle, accepting 2 parameters for length and width:
+    public static double RectangleArea(double length, double width)
+    {
+        return length * width;
+    }
+
+}
