873b73e7c8a5453b2a45ec7afd982045100cff41
Felix Lange <felixlange1@gmail.com>
2024-12-08 16:22:10 -0800

    added getting user input for methods calculating areas of shapes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Getting user input for the radius of the circle. Then parsing the input value (a string) so it becomes a double,
+        // before calling the CircleArea method. The same concept is used for the other methods.
+        Console.WriteLine("To calculate the area of a circle, please enter the radius:");
+        string inputRadius = Console.ReadLine();
+        double radius = double.Parse(inputRadius);
+        Console.WriteLine("The area of your circle is " + (CircleArea(radius)));
+        
+        Console.WriteLine("To calculate the area of a triangle, please enter height:");
+        string inputHeight = Console.ReadLine();
+        double height = double.Parse(inputHeight);
+        Console.WriteLine("Now enter the length of the base:");
+        string inputBase = Console.ReadLine();
+        double baseLength = double.Parse(inputBase);
+        Console.WriteLine("The area of your triangle is " + (TriangleArea(height, baseLength)));
+        
+        Console.WriteLine("To calculate the area of a rectangle, please first enter the width:");
+        string inputWidth = Console.ReadLine();
+        double width = double.Parse(inputWidth);
+        Console.WriteLine("Now enter the length:");
+        string inputLength = Console.ReadLine();
+        double length = double.Parse(inputLength);
+        Console.WriteLine("The area of your rectangle is " + (RectangleArea(length, width)));
+        
+        Console.WriteLine("To calculate the area of a square, please enter the length of the side:");
+        string inputSide = Console.ReadLine();
+        double side = double.Parse(inputSide);
+        Console.WriteLine("The area of your square is " + (SquareArea(side)));
     }
 
     // Method for calculating area of a circle, accepting a radius as parameter:
